ff9857874d935971113b687e0fb1afec9e03f089
Raul Eduardo Espinoza Montoya <respinoza@myevol.biz>
2020-05-27 22:20:00 -0500

    segundo commit

--- DemoWeb/src/main/java/com/example/demo/SecurityConfig.java
+++ DemoWeb/src/main/java/com/example/demo/SecurityConfig.java
@@ -3,35 +3,36 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.demo.service.UserService;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig //extends WebSecurityConfigurerAdapter 
+{
 
-	@Autowired
-	private UserService service;
+//	@Autowired
+//	private UserService service;
 	
 	
-	@Autowired
-	private BCryptPasswordEncoder encoder;
+//	@Autowired
+//	private BCryptPasswordEncoder encoder;
 	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		return bCryptPasswordEncoder;
-		
-	}
+//	@Bean
+//	public BCryptPasswordEncoder passwordEncoder() {
+//		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//		return bCryptPasswordEncoder;
+//		
+//	}
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) 
-			throws Exception{
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) 
+//			throws Exception{
 		
 //		auth.inMemoryAuthentication()
 //			.withUser("user")
@@ -42,23 +43,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //			.password("admin")
 //			.roles("USER","ADMIN");
 
-		auth.userDetailsService(service).passwordEncoder(encoder);
-		
-	}
+//		auth.userDetailsService(service).passwordEncoder(encoder);
+//		
+//	}
 	
 	
-	@Override
-	protected void configure(HttpSecurity http) 
-			throws Exception{
-		
-		http.authorizeRequests()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.httpBasic();
-
-	}
-	
+//	@Override
+//	protected void configure(HttpSecurity http) 
+//			throws Exception{
+//		
+//		http.authorizeRequests()
+//			.anyRequest()
+//			.authenticated()
+//			.and()
+//			.httpBasic();
+//
+//	}
+//	
 	
 	
 }
