30b78e3516076a1caf3c0a99941c61c1aa2fcef2
Raul Eduardo Espinoza Montoya <respinoza@myevol.biz>
2020-05-31 22:35:07 -0500

    actualizacion de fuentes

--- DemoWeb/src/main/java/com/example/demo/controler/DemoController.java
+++ DemoWeb/src/main/java/com/example/demo/controler/DemoController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.Persona;
 //import com.example.demo.repo.IPersonaRepo;
@@ -40,6 +44,32 @@ public class DemoController {
 	}
 	
 	
+	@RequestMapping(value = "/guardar", method = RequestMethod.POST)
+	public String savePersona(@ModelAttribute("persona") Persona persona) {
+	 
+			service.save(persona);
+		
+	    return "redirect:/";
+	}
+	
+	
+	@RequestMapping("/editar/{id}")
+	public ModelAndView showEditPersona(@PathVariable(name = "id") int id) {
+	 
+		ModelAndView mav = new ModelAndView("edit_asesor");
+		
+		Persona persona = service.get(id);
+		mav.addObject("persona", persona);
+		
+	    return mav;
+	}
+	
+	
+	@RequestMapping("/eliminar/{id}")
+	public String showEliminarPersona(@PathVariable(name = "id") int id) {
+		service.delete(id);
+		return "redirect:/";
+	}
 	
 //	@GetMapping("/greeting")
 //	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {		
